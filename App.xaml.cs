d6bfd451147a84908e9e4b170ce246aa175fd58f
Jose-Pater294 <jose.paterninas@cecar.edu.co>
2024-10-14 19:13:05 -0500

    se añadio proyecto base para proyecto

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Prueba2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
